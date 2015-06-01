63c318a443649be12207424666176cab8cfcdd6a
Michael Crump <mbcrump29@gmail.com>
2015-06-01 17:34:50 -0500

    first commit

---
+++ XamarinFormsTelerik/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telerik.Everlive.Sdk.Core.Model.Base;
+
+namespace XamarinFormsTelerik
+{
+	class Customer : DataItem
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Address { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+	}
+}
+
---
+++ XamarinFormsTelerik/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Telerik.Everlive.Sdk.Core;
+using Xamarin.Forms;
+
+namespace XamarinFormsTelerik
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+
+		void submitClicked(object sender, EventArgs args)
+		{
+			SubmitData(entryFirstName.Text, entryLastName.Text, entryAddress.Text, entryCity.Text, entryState.Text, entryZip.Text);
+		}
+
+		private void SubmitData(string strFirstName, string strLastName, string strAddress, string strCity, string strState, string strZip)
+		{
+			var backend = new EverliveApp("Y3R4NJJ5Y3eGh8eT");
+
+			var customerData = backend.WorkWith().Data<Customer>();
+
+			Customer cust = new Customer()
+			{
+				FirstName = strFirstName,
+				LastName = strLastName,
+				Address = strAddress,
+				City = strCity,
+				State = strState,
+				Zip = strZip
+			};
+
+			customerData.Create(cust).ExecuteSync();
+
+		}
+
+		void resetClicked(object sender, EventArgs args)
+		{
+			entryFirstName.Text = null;
+			entryLastName.Text = null;
+			entryAddress.Text = null; 
+			entryCity.Text = null; 
+			entryState.Text = null; 
+			entryZip.Text = null; 
+		}
+
+	}
+}
+
